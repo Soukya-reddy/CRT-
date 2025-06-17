4ff5af03b6f92cb1af3f302dab01d16c8cac1a41
Soukya-reddy <168511853+Soukya-reddy@users.noreply.github.com>
2025-06-17 10:57:02 +0530

    Add files via upload

---
+++ AddElemenrsToHashMap.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class AddElementsToHashMap {
+    public static void main(String args[])
+    {
+        HashMap<Integer, String> hm1 = new HashMap<>();
+        HashMap<Integer, String> hm2
+            = new HashMap<Integer, String>();
+        hm1.put(1, "Geeks");
+        hm1.put(2, "For");
+        hm1.put(3, "Geeks");
+        hm2.put(1, "Geeks");
+        hm2.put(2, "For");
+        hm2.put(3, "Geeks");
+        System.out.println("Mappings of HashMap hm1 are : "
+                           + hm1);
+        System.out.println("Mapping of HashMap hm2 are : "
+                           + hm2);
+    }
+}
